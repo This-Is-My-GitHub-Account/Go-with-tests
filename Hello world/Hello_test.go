75eb8d818b66ac94d5de0869cd83d059e99aede4
Shahane, Sarvesh <sashahane@ptc.com>
2025-01-02 22:29:02 +0530

    added hello world

---
+++ Hello world/Hello_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Prathamesh")
+		want := "Hello, Prathamesh"
+		
+		assertCorrectMessage(t, got, want)
+
+
+	})
+	t.Run("saying hello word when empty string is given", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string)  {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
---
+++ Hello world/go.mod
@@ -0,0 +1,3 @@
+module example.com/Hello
+
+go 1.23.3
